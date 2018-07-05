a7f2e541d2f2a14e3948ab1522d486f97e78ad70
emmcayabyab <emmcayabyab@gmail.com>
2018-07-05 14:25:30 +0800

    sdgsdgsdg

--- model/silver.php
+++ model/silver.php
@@ -1,7 +1,7 @@
 <?php
     require 'fragments/db.php';
 
-    $query = 'SELECT orNumber,IDNumber,lastName,firstName,middleName,address,contactNumber, birthday, age, gender, status, validDate, expiryDate FROM maablist WHERE memType = "Premier Bronze" ORDER BY validDate DESC';
+    $query = 'SELECT orNumber,IDNumber,lastName,firstName,middleName,address,contactNumber, birthday, age, gender, status, validDate, expiryDate FROM maablist WHERE memType = "Premier Silver" ORDER BY validDate DESC';
     $result = mysqli_query($db, $query);
 
     $members = [];
