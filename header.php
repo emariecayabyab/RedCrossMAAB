1411739b9260b61eaa6e13eadf7a74844cc76984
JannMichael <jannmichaeltoledo@yahoo.com>
2018-07-10 10:34:08 +0800

    Remove Search, Home Link

--- header.php
+++ header.php
@@ -5,17 +5,11 @@
                     <!-- Logo -->
                     <a class="logo" href="registration.php">
                         <!-- Logo icon image, you can use font-icon also --><b>
-                        <!--This is dark logo icon--><img src="../plugins/images/logo.jpg" alt="home" /><!--This is light logo icon--><!--img src="../plugins/images/admin-logo-dark.png" alt="home" class="light-logo" /-->
+                        <!--This is dark logo icon--><img src="../plugins/images/logo.jpg" /><!--This is light logo icon--><!--img src="../plugins/images/admin-logo-dark.png" alt="home" class="light-logo" /-->
                      </b>
                    </a>
                 </div>
                 <!-- /Logo -->
-                <ul class="nav navbar-top-links navbar-right pull-right">
-                    <li>
-                        <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
-                            <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a> </form>
-                    </li>
-                </ul>
 				<link rel="icon" type="image/jpg" sizes="16x16" href="../plugins/images/logo.jpg">
             </div>
                 <!-- /Logo -->
