75fd099bfb65b5b1702a2072b5e44de505b93817
JannMichael <jannmichaeltoledo@yahoo.com>
2018-07-21 17:48:51 +0800

    Updated Delete

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ require 'fragments/db.php';
 
  $query = "DELETE FROM maablist WHERE IDNumber ='$_GET[id]'";
  if(mysqli_query($db, $query)){
- 	header("Location: index.php");
+ 	header("Location: registration.php");
  } else {
  	echo 'failed';
  }
